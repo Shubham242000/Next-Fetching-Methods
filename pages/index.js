48e5e7448a1513e6cd744e097ec25c4b9ed62e03
Shubham Shrivastava <shubhamshrivastava2000@gmail.com>
2022-06-01 23:32:45 +0530

    NEXTAUTH_URL updated

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ const Home = () => {
       <h1>Home Page</h1>
       <hr />
       <h4>
-        An application that depicts various fetching method that NextJs
+        An Application that depicts various fetching method that NextJs
         provides, NextJs APIs, Context API, Authentication using next-auth etc
       </h4>
     </div>
